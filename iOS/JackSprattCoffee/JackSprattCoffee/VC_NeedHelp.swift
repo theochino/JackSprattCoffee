f95e76e5db45df223f748eb82a45412a96ed7178
theochino <theo@Theos-MacBook-Air.local>
2016-12-22 05:37:44 -0500

    Initinal Commit of "A" code just to put something. Need to work on segues:)

---
+++ iOS/JackSprattCoffee/JackSprattCoffee/VC_NeedHelp.swift
@@ -0,0 +1,24 @@
+//
+//  VC_NeedHelp.swift
+//  JackSprattCoffee
+//
+//  Created by Theo Chino on 12/22/16.
+//  Copyright © 2016 TheoChino. All rights reserved.
+//
+
+import UIKit
+
+class VC_NeedHelp: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
